1f5e5a31e89dceea1dae39d3e5f996043183d539
dhirendraagrawal <64911095+dhirendraagrawal@users.noreply.github.com>
2020-05-12 14:42:22 -0400

    Added
    Update

--- src/main/java/com/example/demo/controller/MainRestController.java
+++ src/main/java/com/example/demo/controller/MainRestController.java
@@ -7,6 +7,7 @@ import com.example.demo.model.Employee;
 import com.example.demo.service.EmployeeService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableDiscoveryClient
 public class MainRestController {
 
 	@Autowired
